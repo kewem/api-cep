bb606af53687a1ce7ca253739da0b5cf9d8dc78a
Alan Kewem <kewemcg@gmail.com>
2022-05-23 21:46:45 -0400

    feat: create User entity

---
+++ src/entities/User.ts
@@ -0,0 +1,20 @@
+import { randomUUID } from "crypto";
+import { sign } from "jsonwebtoken";
+
+export class User {
+  public readonly id: string;
+  public readonly jwt: string;
+  public name: string;
+  public email: string;
+
+  constructor(props: [Omit<User, "id">, Omit<User, "jwt">], id?: string, jwt?: string) {
+    Object.assign(this, props);
+
+    if (!id) {
+      this.id = randomUUID();
+    }
+    if (!jwt) {
+      this.jwt = sign({ userId: this.id }, "secret_private_key");
+    }
+  }
+}
