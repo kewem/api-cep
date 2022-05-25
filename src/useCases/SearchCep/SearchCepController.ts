df88022baeb82fcc4c635d9be54e5dc1b5361db4
Alan Kewem <kewemcg@gmail.com>
2022-05-25 17:39:26 -0400

    feat: controller to handle CEP search

---
+++ src/useCases/SearchCep/SearchCepController.ts
@@ -0,0 +1,16 @@
+import { NextFunction, Request, Response } from "express";
+import { SearchCepUseCase } from "./SearchCepUseCase";
+
+export class SearchCepController {
+  constructor(private searchCepUseCase: SearchCepUseCase) {}
+
+  async handle(req: Request, res: Response, next: NextFunction): Promise<Response> {
+    const { cep } = req.params;
+
+    const result = await this.searchCepUseCase.execute({ cep }).catch((error) => {
+      res.status(400).json({ error: error });
+    });
+
+    return res.status(200).json(result);
+  }
+}
