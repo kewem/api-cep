6a0863e9f1b6f0ab20a5b576475d62b6140a4591
Alan Kewem <kewemcg@gmail.com>
2022-05-25 17:36:55 -0400

    feat: define in interface wich methods are required in implementation of CEP provider

---
+++ src/providers/ICepProvider.ts
@@ -0,0 +1,7 @@
+import { Cep } from "../entities/Cep";
+
+export interface ICepProvider {
+  findCep(cep: string): Promise<Cep>;
+  sanitizeCep(cep: string): string;
+  validateCep(cep: string): Boolean;
+}
