4cff0f0efb975617c55dacff453e05bb5c12f6fb
Alan Kewem <kewemcg@gmail.com>
2022-05-25 18:25:09 -0400

    feat: cache provider implementation

---
+++ src/providers/Cache/implementations/CacheProvider.ts
@@ -0,0 +1,21 @@
+import Cache from "node-cache";
+import { ICacheProvider } from "../ICacheProvider";
+
+class CacheProvider implements ICacheProvider {
+  private cache = new Cache();
+  private ttl = 300;
+
+  public setItemOnCache(key: string, value: Object) {
+    return this.cache.set(key, value, this.ttl);
+  }
+
+  public getItemOfCache(key: string) {
+    return this.cache.get(key);
+  }
+
+  public hasItemOnCache(key: string) {
+    return this.cache.has(key);
+  }
+}
+
+const cacheProvider = new CacheProvider();
