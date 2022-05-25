3930a74b434b84c0e0a1a6e51bb356420bf708d9
Alan Kewem <kewemcg@gmail.com>
2022-05-25 17:39:02 -0400

    feat: useCase what make a search by CEP

---
+++ src/useCases/SearchCep/SearchCepUseCase.ts
@@ -0,0 +1,14 @@
+import { ICepProvider } from "../../providers/ICepProvider";
+import { ISearchCepDTO } from "./SearchCepDTO";
+
+export class SearchCepUseCase {
+  constructor(private cepProvider: ICepProvider) {}
+
+  async execute(data: ISearchCepDTO) {
+    const cep = await this.cepProvider.findCep(data.cep).catch((error) => {
+      return Promise.reject(error);
+    });
+
+    return Promise.resolve(cep);
+  }
+}
