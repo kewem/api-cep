7e0cf440c3c500085f3ccfd88c6aa2918bac1232
Alan Kewem <kewemcg@gmail.com>
2022-05-25 23:59:07 -0400

    feat: inclusion of caching service in SearchCepUseCase

--- src/useCases/SearchCep/SearchCepUseCase.ts
+++ src/useCases/SearchCep/SearchCepUseCase.ts
@@ -1,14 +1,28 @@
+import { ICacheProvider } from "../../providers/Cache/ICacheProvider";
 import { ICepProvider } from "../../providers/Cep/ICepProvider";
 import { ISearchCepDTO } from "./SearchCepDTO";
 
 export class SearchCepUseCase {
-  constructor(private cepProvider: ICepProvider) {}
+  constructor(private cepProvider: ICepProvider, private cacheProvider: ICacheProvider) {}
 
   async execute(data: ISearchCepDTO) {
-    const cep = await this.cepProvider.findCep(data.cep).catch((error) => {
+    if (!this.cepProvider.validateCep(data.cep)) {
+      return Promise.reject("Invalid CEP. expected something like: xxxxx-xxx");
+    }
+
+    const sanitizedCep = this.cepProvider.sanitizeCep(data.cep);
+
+    if (this.cacheProvider.hasItemOnCache(sanitizedCep)) {
+      const cep = this.cacheProvider.getItemOfCache(sanitizedCep);
+      return Promise.resolve({ ...cep, fromCache: true });
+    }
+
+    const cep = await this.cepProvider.findCep(sanitizedCep).catch((error) => {
       return Promise.reject(error);
     });
 
-    return Promise.resolve(cep);
+    this.cacheProvider.setItemOnCache(sanitizedCep, cep);
+
+    return Promise.resolve({ ...cep, fromCache: false });
   }
 }
