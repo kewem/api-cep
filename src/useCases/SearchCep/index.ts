57be0869df1f8e19b0483b5ef08ab921598f1dac
Alan Kewem <kewemcg@gmail.com>
2022-05-26 00:00:58 -0400

    feat: caching provider instance injection in SearchCepUseCase

--- src/useCases/SearchCep/index.ts
+++ src/useCases/SearchCep/index.ts
@@ -1,10 +1,11 @@
 import { CepProvider } from "../../providers/Cep/implementations/CepProvider";
+import { cacheProvider } from "../../providers/Cache/implementations/CacheProvider";
 import { SearchCepController } from "./SearchCepController";
 import { SearchCepUseCase } from "./SearchCepUseCase";
 
 const cepProvider = new CepProvider();
 
-const searchCepUseCase = new SearchCepUseCase(cepProvider);
+const searchCepUseCase = new SearchCepUseCase(cepProvider, cacheProvider);
 
 const searchCepController = new SearchCepController(searchCepUseCase);
 
